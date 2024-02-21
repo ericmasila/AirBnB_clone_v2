83f8bf326c5657a7599890d389ab5c21475015ff
Victor <victormwendwa804@gmail.com>
2024-02-21 03:24:27 -0800

    Base model commit

--- models/base_model.py
+++ models/base_model.py
@@ -10,61 +10,60 @@ Base = declarative_base()
 
 
 class BaseModel:
-"""A base class for all hbnb models"""
-id = Column(String(60), nullable=False, primary_key=True)
-created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
-def __init__(self, *args, **kwargs):
-"""Instatntiates a new model"""
-if not kwargs:
-from models import storage
-self.id = str(uuid.uuid4())
-self.created_at = datetime.now()
-self.updated_at = datetime.now()
-else:
-if kwargs.get('updated_at'):
-kwargs['updated_at'] = datetime.strptime(
-kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-else:
-self.updated_at = datetime.utcnow()
-if kwargs.get('created_at'):
-kwargs['created_at'] = datetime.strptime(
-kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-else:
-self.created_at = datetime.utcnow()
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            if kwargs.get('updated_at'):
+                kwargs['updated_at'] = datetime.strptime(
+                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.updated_at = datetime.utcnow()
+                if kwargs.get('created_at'):
+                    kwargs['created_at'] = datetime.strptime(
+                            kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    self.created_at = datetime.utcnow()
 
-for key, val in kwargs.items():
-if '__class__' not in key:
-setattr(self, key, val)
-if not self.id:
-self.id = str(uuid.uuid4())
+                for key, val in kwargs.items():
+                    if '__class__' not in key:
+                        setattr(self, key, val)
+                if not self.id:
+                    self.id = str(uuid.uuid4())
+        
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+    
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
 
-def __str__(self):
-"""Returns a string representation of the instance"""
-cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = dict(self.__dict__)
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
 
-def save(self):
-"""Updates updated_at with current time when instance is changed"""
-from models import storage
-self.updated_at = datetime.now()
-storage.new(self)
-storage.save()
+        if hasattr(self, '_sa_instance_state'):
+            del dictionary['_sa_instance_state']
+        return dictionary
 
-def to_dict(self):
-"""Convert instance into dict format"""
-dictionary = dict(self.__dict__)
-dictionary['__class__'] = self.__class__.__name__
-dictionary['created_at'] = self.created_at.isoformat()
-dictionary['updated_at'] = self.updated_at.isoformat()
-
-if hasattr(self, '_sa_instance_state'):
-del dictionary['_sa_instance_state']
-return dictionary
-
-def delete(self):
-''' Deletes an object '''
-from models import storage
-storage.delete(self)
-
+    def delete(self):
+        ''' Deletes an object '''
+        from models import storage
+        storage.delete(self)
