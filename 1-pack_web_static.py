970958982bbab75fb742f79033811e952ae2c2c4
ericmasila <ericmasila254@gmail.com>
2024-03-08 03:51:47 -0800

    pack web

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-'''
-A script to generate .tgz file from the contents of webstatic
-usage: fab -f 1-pack_web_static.py do_pack
-'''
-from fabric.api import local
-from time import strftime
+"""
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
+
+from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
-    '''Generate required files'''
-    timenow = strftime('%Y%M%d%H%M%S')
-    try:
-        local('mkdir -p versions')
-        filename = 'versions/web_static_{}.tgz'.format(timenow)
-        local('tar -czvf {} web_static/'.format(filename))
-        return filename
-    except Exception:
+    """
+    making an archive on web_static folder
+    """
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None
